63464acccfb05f353bf595efcf637ffded43e962
lazycece <lazycece@gmail.com>
2026-09-02 18:12:28 +0800

    tools cp

--- tools/spotless_apply.sh
+++ tools/spotless_apply.sh
@@ -1,5 +1,5 @@
 #
-#    Copyright 2023 lazycece<lazycece@gmail.com>
+#    Copyright 2026 lazycece<lazycece@gmail.com>
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 #    limitations under the License.
 #
 
-mvn clean deploy -pl tradecore-facade -am
+mvn spotless:apply
